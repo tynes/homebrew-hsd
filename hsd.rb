d606639da1377df833b9476431084d850a9ce97c
Mark Tyneway <mark@purse.io>
2020-03-02 14:28:57 -0800

    hsd: fix audit errors

--- hsd.rb
+++ hsd.rb
@@ -7,8 +7,8 @@ class Hsd < Formula
   sha256 "6a519a6093481b60c04944528ff28752ccac00694c3e47635fa941d42bb7970b"
 
   depends_on "node@10"
+  depends_on "python" => :build
   depends_on "unbound"
-  depends_on "python" => "build"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
