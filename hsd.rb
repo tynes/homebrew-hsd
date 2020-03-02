856fc98b76d61413fda0c9c90a873868a3098d19
Mark Tyneway <mark@purse.io>
2020-03-02 14:30:16 -0800

    hsd: fix audit error

--- hsd.rb
+++ hsd.rb
@@ -6,8 +6,8 @@ class Hsd < Formula
   url "https://github.com/handshake-org/hsd/archive/v2.0.2.tar.gz"
   sha256 "6a519a6093481b60c04944528ff28752ccac00694c3e47635fa941d42bb7970b"
 
-  depends_on "node@10"
   depends_on "python" => :build
+  depends_on "node@10"
   depends_on "unbound"
 
   def install
